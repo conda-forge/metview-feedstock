3fdde5dd66ca6ddba81675f4e74c34d7aea9cd41
Iain Russell <Iain.Russell@ecmwf.int>
2019-09-21 11:54:41 +0100

    Remove unnecessary patch and only run Metview tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -63,5 +63,7 @@ cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
 
 make -j $CPU_COUNT VERBOSE=1
 
-ctest --output-on-failure -j $CPU_COUNT -I test_list.txt
+cd metview
+#ctest --output-on-failure -j $CPU_COUNT -I test_list.txt
+ctest --output-on-failure -j $CPU_COUNT
 make install
