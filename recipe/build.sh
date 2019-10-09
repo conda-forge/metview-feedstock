22858b75e2a3a08eb3dabfa09a441df36280cb15
Iain Russell <Iain.Russell@ecmwf.int>
2019-10-09 10:13:06 +0100

    Fix the list of tests to be ignored for Metview 5.7.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -25,7 +25,7 @@ mkdir ../build && cd ../build
 # build process. We generate a list of tests to pass to ctest by skipping the failing ones.
 # This should be removed once the tests are fixed internally at ECMWF.
 if [[ $(uname) == Linux ]]; then
-    export TESTS_TO_SKIP="NONE"
+    export TESTS_TO_SKIP="999"  # dummy value
 elif [[ $(uname) == Darwin ]]; then
     # 25: inline_c.mv_dummy_target (not surprising and not important for 99% of people)
     # 36: geopoints.mv_dummy_target (only fails on macos on conda)
